498426b161c809b1dc1664296278f6a8aa6de037
Mefit Hernández <mefitdev@gmail.com>
2024-05-02 22:40:15 -0700

    [dev] Update next  env variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "delicious-vicious.s3.us-east-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+  env: {
+    GRAPHQL_URI: process.env.GRAPHQL_URI,
+  },
+};
 
 export default nextConfig;

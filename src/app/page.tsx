3167cb73782ff702c96d9f04bb8e4fd3f5614b16
Mefit Hernández <mefitdev@gmail.com>
2024-07-20 12:04:08 -0600

    [main] Main page edits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@
 import { SimpleGrid, Box, Flex, Title, Text } from "@mantine/core";
 
 import classes from "./page.module.css";
-// import { useSuspenseQuery } from "@apollo/client";
-// import { GET_BANNERS, GetBannersReponse } from "@/lib/graphql/general_queries";
-// import BannerCarousel from "@/components/BannerCarousel";
-// import { Suspense } from "react";
+import { useSuspenseQuery } from "@apollo/client";
+import { GET_BANNERS, GetBannersReponse } from "@/lib/graphql/general_queries";
+import BannerCarousel from "@/components/BannerCarousel";
+import { Suspense } from "react";
 import { bucketStaticPath } from "@/lib/constants";
 import dynamic from "next/dynamic";
 
@@ -14,34 +14,33 @@ const CachedImage = dynamic(() => import("@/components/shared/CachedImage"), {
 });
 
 export default function Page() {
-  // const {
-  //   data: { portadas: banners },
-  // }: GetBannersReponse = useSuspenseQuery(GET_BANNERS, {
-  //   variables: {
-  //     where: { es_visible: { equals: true } },
-  //   },
-  // });
+  const {
+    data: { portadas: banners },
+  }: GetBannersReponse = useSuspenseQuery(GET_BANNERS, {
+    variables: {
+      where: { es_visible: { equals: true } },
+    },
+  });
 
   return (
     <>
-      {/* <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<div>Loading...</div>}>
         {banners.length !== 0 && <BannerCarousel items={banners} />}
-      </Suspense> */}
+      </Suspense>
       <SimpleGrid cols={{ sm: 2, xs: 1 }} spacing="0">
-        <Flex className={classes.card} p="md" px={80}>
-          <Title order={1}>Eat</Title>
+        <Flex className={classes.card}>
+          <Title>¡Bienvenidos a Delicious Vicious!</Title>
           <Text size="xl">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque
-            expedita libero tempora aspernatur rem culpa officia, nihil
-            praesentium, suscipit nam nesciunt ut maiores iusto deleniti odio
-            corporis exercitationem fugiat voluptates?
+            Nos apasiona crear galletas estilo NY y brownies decadentes que te
+            dejarán sin aliento. Cada bocado está lleno de sabor y textura, ¡te
+            garantizamos que te dejarán queriendo más!
           </Text>
         </Flex>
         <Box pos="relative" mih={500}>
           <CachedImage
             isStatic
             style={{ objectFit: "cover" }}
-            src={`${bucketStaticPath}/COOKIE_BOX_CLOSED.webp`}
+            src={`${bucketStaticPath}/ny_style.webp`}
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             alt="Cookie box"
           />
@@ -52,29 +51,27 @@ export default function Page() {
           <CachedImage
             isStatic
             style={{ objectFit: "cover" }}
-            src={`${bucketStaticPath}/COOKIE_BOX_CLOSEUP.webp`}
+            src={`${bucketStaticPath}/anytime.webp`}
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             alt="Cookie box"
           />
         </Box>
-        <Flex className={classes.card} p="md" px={80}>
-          <Title order={1}>Drink</Title>
+        <Flex className={classes.card} px={80}>
+          <Title>Perfectas para cada ocasión</Title>
           <Text size="xl">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque
-            expedita libero tempora aspernatur rem culpa officia, nihil
-            praesentium, suscipit nam nesciunt ut maiores iusto deleniti odio
-            corporis exercitationem fugiat voluptates?
+            Nuestras delicias son perfectas para cualquier ocasión, desde
+            celebraciones hasta simplemente darse un capricho después de un
+            largo día.
           </Text>
         </Flex>
       </SimpleGrid>
       <SimpleGrid cols={{ sm: 2, xs: 1 }} spacing="0">
-        <Flex className={classes.card} p="md" px={80}>
-          <Title order={1}>Enjoy</Title>
+        <Flex className={classes.card} px={80}>
+          <Title>Únete a nosotros</Title>
           <Text size="xl">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque
-            expedita libero tempora aspernatur rem culpa officia, nihil
-            praesentium, suscipit nam nesciunt ut maiores iusto deleniti odio
-            corporis exercitationem fugiat voluptates?
+            Sweetaholics orgullosos! Abrazamos nuestra pasión por los sabores
+            caramelizados, los esponjosos brownies y las tentadoras texturas.
+            Sabemos que es un amor incondicional y elegimos no resistirnos a el.
           </Text>
         </Flex>
         <Box pos="relative" mih={500}>
@@ -82,7 +79,7 @@ export default function Page() {
             isStatic
             style={{ objectFit: "cover" }}
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            src={`${bucketStaticPath}/LOGO_WITH_CAT.webp`}
+            src={`${bucketStaticPath}/join_us.webp`}
             alt="Cookie box"
           />
         </Box>
